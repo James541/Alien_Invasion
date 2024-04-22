e0b7e53e9096e817294e9af54085d83c9fd8fc8c
James541 <96335020+James541@users.noreply.github.com>
2024-04-21 23:31:25 -0700

    added movement range on far left and right sides of the screen

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        self.screent_rect = ai_game.screen.get_rect()
+        self.screen_rect = ai_game.screen.get_rect()
 
 
         # load the ship image and get its rect.
@@ -15,7 +15,10 @@ class Ship:
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen.
-        self.rect.midbottom = self.screent_rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        #Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
 
 
 
@@ -25,10 +28,13 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on movement flags."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        #Update rect object from self.x
+        self.rect.x = self.x
 
     
     def blitme(self):
