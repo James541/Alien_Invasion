def52d91b76849d3c5b65559aef2ec6b604deb9d
James541 <96335020+James541@users.noreply.github.com>
2024-04-30 01:26:24 -0700

    aliens change directions and move down on edge hit complete

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,7 +82,8 @@ class AlienInvasion:
         self.bullets.update()
 
     def _update_aliens(self):
-        """Update the position of all aliens in the fleet"""
+        """heck if the fleet is at an edge, then update the position of all aliens in the fleet"""
+        self._check_fleet_edges()
         self.aliens.update()
 
         # Get rid of bullets that have disappeared.
@@ -122,6 +123,12 @@ class AlienInvasion:
             if alien.check_edges():
                 self._change_fleet_direction()
                 break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleets direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
