f6ef8d6adbe3cfd28b51ff1c1cf4e8ad79fe0ffc
James541 <96335020+James541@users.noreply.github.com>
2024-05-01 00:20:28 -0700

    added alien and ground collision, limited number of ships

--- settings.py
+++ settings.py
@@ -8,6 +8,7 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        self.ship_limit = 3
         # Bullet settings
         self.bullet_speed = 4.0
         self.bullet_width = 750
@@ -16,6 +17,6 @@ class Settings:
         self.bullets_allowed = 3
         # Alien settings.
         self.alien_speed = 1
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 50
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
