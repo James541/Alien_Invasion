3ecbd071c0a60dacc964c4cdee8c5a7eddc0672a
James541 <96335020+James541@users.noreply.github.com>
2024-05-05 23:26:04 -0700

    made mouse invisible when game active, returns when game inactive

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,6 +85,9 @@ class AlienInvasion:
             # Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
                    
 
     def _check_keydown_events(self,event):
@@ -203,6 +206,7 @@ class AlienInvasion:
             # Pause
             sleep(2)
         else: self.game_active = False
+        pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
