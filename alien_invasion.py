829c29e4646951945ab4a951c6ce4861ad2d5032
James541 <96335020+James541@users.noreply.github.com>
2024-05-06 22:25:34 -0700

    added speed up scale and seperated static and dynamic settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,6 +74,9 @@ class AlienInvasion:
         """Start a new game when the player clicks Play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            # Rest the game settings.
+            self.settings.initialize_dynamic_settings()
+
             # Reset game statistics.
             self.stats.reset_stats()
             self.game_active = True
@@ -134,6 +137,7 @@ class AlienInvasion:
             # Destroy existing bullets and create a new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
 
     def _update_aliens(self):
