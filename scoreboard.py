2c748bfb2534be3e73db57e5d02f9cb9f2d6f33a
James541 <96335020+James541@users.noreply.github.com>
2024-05-09 23:58:54 -0700

    edit

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,7 @@
 import pygame.font
 
 class Scoreboard:
-    """A class to report scoring information."""
+    """A class to report scoring information"""
 
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes."""
