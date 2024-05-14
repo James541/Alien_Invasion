6fc0f2865892b05dffa7889d55eec8226451fd76
James541 <96335020+James541@users.noreply.github.com>
2024-05-14 01:15:06 -0700

    added ships left images, book guidance completed, version 1.0

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,6 +84,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True
 
             #Get rid of any remaining bullets and aliens.
@@ -212,13 +213,12 @@ class AlienInvasion:
         self._update_screen()
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships left.
+            # Decrement ships left, and update scoreboard.
             self.stats.ships_left -= 1
-
+            self.sb.prep_ships()
             # Get rid of any remaioning bullets and aliens.
             self.bullets.empty()
             self.aliens.empty()
-
             # Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()
